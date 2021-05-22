c49a595dd8927bffe57e37e9c2b3baa4ebbf48e7
Kristof Eekhaut <kristof.eekhaut@ordina.be>
2021-05-22 10:56:40 +0200

    Initial project setup

---
+++ src/test/java/be/eekhaut/kristof/aws/events/event/EventApplicationTests.java
@@ -0,0 +1,13 @@
+package be.eekhaut.kristof.aws.events.event;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
